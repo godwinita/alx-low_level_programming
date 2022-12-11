018777416e0665f3b3c216502787358a341e89b9
root <hellogeetech@gmail.com>
2022-12-11 04:50:41 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like builidng a multiligual puzzle");
+	return (0);
+
+}
