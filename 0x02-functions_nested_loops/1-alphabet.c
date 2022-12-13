38bd9c6b7197be6c6ac34459ed4c2b6419c04808
root <hellogeetech@gmail.com>
2022-12-13 05:41:46 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints the alp[habet in lowercase, followed by a new line
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; lettetr++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
